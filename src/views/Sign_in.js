0452b80014ba8972a19faa7ac7cd787e02cfe62a
Gustavo Barreiro <barreirogustavo05@gmail.com>
2021-07-04 17:49:53 -0300

    implementando suscriber front

--- src/views/Sign_in.js
+++ src/views/Sign_in.js
@@ -1,8 +1,34 @@
 import React, {useState} from "react";
 import { Link, useHistory } from "react-router-dom";
 import axios from "axios";
-import '../styles/Signin_Signup.css'
+import $ from 'jquery';
+import TweenMax from 'scrollmagic-plugin-gsap'
+import Sine from 'sine-waves'
+import '../styles/Sign_in.css'
 
+
+$('#login-button').click(function(){
+  $('#login-button').fadeOut("slow",function(){
+    $("#container").fadeIn();
+    TweenMax.from("#container", .4, { scale: 0, ease:Sine.easeInOut});
+    TweenMax.to("#container", .4, { scale: 1, ease:Sine.easeInOut});
+  });
+});
+
+$(".close-btn").click(function(){
+  TweenMax.from("#container", .4, { scale: 1, ease:Sine.easeInOut});
+  TweenMax.to("#container", .4, { left:"0px", scale: 0, ease:Sine.easeInOut});
+  $("#container, #forgotten-container").fadeOut(800, function(){
+    $("#login-button").fadeIn(800);
+  });
+});
+
+/* Forgotten Password */
+$('#forgotten').click(function(){
+  $("#container").fadeOut(function(){
+    $("#forgotten-container").fadeIn();
+  });
+});
 
 const Login = () => {
   const history = useHistory();
@@ -32,35 +58,42 @@ const Login = () => {
     }
 
   return (
-    <form id="form-signin-signup">
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Username</label>
-          <input
-                  type="text"
-                  placeholder="Intruduzca su username"
-                  class="form-control"
-                  value={nickname}
-                  onChange={(e) => setnickname(e.target.value)}
-                />
-        </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Password</label>
-          <input
-                  type="password"
-                  placeholder="Intruduzca su password"
-                  value={password}
-                  class="form-control"
-                  id="exampleInputPassword1"
-                  onChange={(e) => setpassword(e.target.value)}
-                />
-        </div>
-        <div class="mb-3 form-check">
-          <h6 id="H5Register">
-            <Link id="linkRegister" to="/register">Registrarse</Link>
-          </h6>
-        </div>
-        <Link id="form-button " type="submit" class="btn btn-primary" onClick={() => PostData()}>Ingresar</Link>
-      </form>
+    <div>
+
+      <div id="login-button">
+    <img src="https://dqcgrsy5v35b9.cloudfront.net/cruiseplanner/assets/img/icons/login-w-icon.png">
+    </img>
+  </div>
+  <div id="container">
+    <h1>Log In</h1>
+    <span class="close-btn">
+      <img src="https://cdn4.iconfinder.com/data/icons/miu/22/circle_close_delete_-128.png"/>
+    </span>
+
+    <form>
+      <input type="email" name="email" placeholder="E-mail"/>
+      <input type="password" name="pass" placeholder="Password"/>
+      <a href="#">Log in</a>
+      <div id="remember-container">
+        <input type="checkbox" id="checkbox-2-1" class="checkbox" checked="checked"/>
+        <span id="remember">Remember me</span>
+        <span id="forgotten">Forgotten password</span>
+      </div>
+  </form>
+  </div>
+
+  <div id="forgotten-container">
+    <h1>Forgotten</h1>
+    <span class="close-btn">
+      <img src="https://cdn4.iconfinder.com/data/icons/miu/22/circle_close_delete_-128.png"></img>
+    </span>
+
+    <form>
+      <input type="email" name="email" placeholder="E-mail"/>
+      <a href="#" class="orange-btn">Get new password</a>
+  </form>
+  </div>
+    </div>
   )
     };
 
