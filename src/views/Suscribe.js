43de8947fabfff947fdc9e64aaca56b9057b093e
llaghezza <llaghezza@veritran.com>
2021-07-04 23:49:49 -0300

    se agregar para cambiar el url de norificacion de subscripcion en la pantalla subscribe

--- src/views/Suscribe.js
+++ src/views/Suscribe.js
@@ -1,14 +1,15 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 const Suscribe = () => {
-    const [searchText, setSearchText] = useState("")
+    const [searchText, setSearchText] = useState('')
+    const [urlNotify, setUrlNotify] = useState('')
     const [searchResult, setSearchResult] = useState([])
     const [filteredResult, setFilteredResult] = useState([])
     
-    const token = JSON.stringify(localStorage.getItem("apiKey"))
+    const token = JSON.stringify(localStorage.getItem('apiKey'))
 
     const filterResult = () => {
-        setFilteredResult(searchResult.filter(title => title.originalTitle.includes(searchText)))
+        setFilteredResult(searchResult.filter(title => title.originalTitle.toLocaleLowerCase().includes(searchText.toLocaleLowerCase())))
     }
 
     useEffect(() => {
@@ -50,13 +51,55 @@ const Suscribe = () => {
             .catch((err) => {
                 console.log(err);
             });
-    
     }
+
+    const unsubscribe = (tconst) => {
+        fetch("http://localhost:8080/title/unsubscribe", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+                'Access-Control-Allow-Origin': '*',
+                'api-key': JSON.stringify(localStorage.getItem("apiKey"))
+            }, 
+            body: JSON.stringify({
+                tconst : tconst,
+                nick: localStorage.getItem("nickname")
+            }),
+        })
+            .then((res) => console.log(res.json()))
+            .then(response => console.log(response))
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    const notifyUrl = () => {
+        fetch("http://localhost:8080/users/notifyurl", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+                'Access-Control-Allow-Origin': '*',
+                'api-key': JSON.stringify(localStorage.getItem("apiKey"))
+            }, 
+            body: JSON.stringify({
+                notify_url : urlNotify,
+                nickname: localStorage.getItem("nickname")
+            }),
+        })
+            .then((res) => console.log(res.json()))
+            .then(response => console.log(response))
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
     return (
         <div>
             <form class="d-flex">
                 <input class="form-control me-2" type="search" placeholder="Search" onChange={(e) => setSearchText(e.target.value)} aria-label="Search" />
                 <button class="btn btn-outline-success" type="submit" onClick={(e) => { search(e) }}>Search</button>
+                <input class="form-control me-2" type="url" placeholder="URL to notify subscribe" onChange={(e) => setUrlNotify(e.target.value)} aria-label="Url to notify"/>
+                <button class="btn btn-outline-success" type="submit" onClick={(e) => { notifyUrl() }}>Set</button>
             </form>
             {
                 searchResult.length > 0 ?
@@ -68,7 +111,8 @@ const Suscribe = () => {
                                         <h5 class="card-title">{tittle.originalTitle}</h5>
                                         <h6 class="card-subtitle mb-2 text-muted">{tittle.titleType}</h6>
                                         <p class="card-text">{tittle.genres}</p>
-                                        <button type="button" class="btn btn-primary" onClick={() => suscribe(tittle.tconst)}>Suscribe</button>
+                                        <button type="button" class="btn btn-primary" onClick={() => suscribe(tittle.tconst)}>Subscribe</button>
+                                        <button type="button" class="btn btn-secondary" onClick={() => unsubscribe(tittle.tconst)}>Unsubscribe</button>
                                     </div>
                                 </div>
                             </div>)
